6f18557f5571e34a280c31ce9a3be754c16e5835
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-09-08 07:43:07 +0200

    Small infrastructure and an empty solution for the Visual Studio package.

---
+++ src/VSX/Twainsoft.SolutionRenamer.VSPackage/Guids.cs
@@ -0,0 +1,15 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace Twainsoft.Twainsoft_SolutionRenamer_VSPackage
+{
+    static class GuidList
+    {
+        public const string guidTwainsoft_SolutionRenamer_VSPackagePkgString = "140b0282-2aaa-4427-a545-4fd4452656af";
+        public const string guidTwainsoft_SolutionRenamer_VSPackageCmdSetString = "3ea51fcb-84e7-4277-98f1-609cb500857e";
+        public const string guidToolWindowPersistanceString = "329dddf7-8d30-46e9-997b-d9863990df74";
+
+        public static readonly Guid guidTwainsoft_SolutionRenamer_VSPackageCmdSet = new Guid(guidTwainsoft_SolutionRenamer_VSPackageCmdSetString);
+    };
+}
