91628e96c5086df8fd3dd33e7162f4ea92fdc7d4
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-09-10 12:07:06 +0200

    Played with various events for the process of renaming a project. Works in both the Addin and Package way.

--- src/VSX/Twainsoft.SolutionRenamer.VSPackage/VSX/SolutionRenamer.cs
+++ src/VSX/Twainsoft.SolutionRenamer.VSPackage/VSX/SolutionRenamer.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 using EnvDTE;
 using EnvDTE80;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 using Twainsoft.SolutionRenamer.VSPackage.GUI;
@@ -15,16 +16,16 @@ namespace Twainsoft.SolutionRenamer.VSPackage.VSX
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [ProvideToolWindow(typeof(MyToolWindow))]
     [Guid(GuidList.guidTwainsoft_SolutionRenamer_VSPackagePkgString)]
-    public sealed class SolutionRenamer : Package
+    public sealed class SolutionRenamer : Package, IVsSolutionEvents, IVsSolutionEvents4
     {
         private void OnShowToolWindow(object sender, EventArgs e)
         {
-            ToolWindowPane window = this.FindToolWindow(typeof(MyToolWindow), 0, true);
+            var window = this.FindToolWindow(typeof(MyToolWindow), 0, true);
             if ((null == window) || (null == window.Frame))
             {
                 throw new NotSupportedException(Resources.Resources.CanNotCreateWindow);
             }
-            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            var windowFrame = (IVsWindowFrame)window.Frame;
             Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
         }
 
@@ -35,38 +36,120 @@ namespace Twainsoft.SolutionRenamer.VSPackage.VSX
             base.Initialize();
 
             // Add our command handlers for menu (commands must exist in the .vsct file)
-            OleMenuCommandService mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
+            var mcs = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
             if ( null != mcs )
             {
                 // Create the command for the menu item.
-                CommandID menuCommandID = new CommandID(GuidList.guidTwainsoft_SolutionRenamer_VSPackageCmdSet, (int)PkgCmdIDList.cmdidMyCommand);
+                var menuCommandID = new CommandID(GuidList.guidTwainsoft_SolutionRenamer_VSPackageCmdSet, (int)PkgCmdIDList.cmdidMyCommand);
                 //MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID );
                 //mcs.AddCommand( menuItem );
                 // Create the command for the tool window
-                CommandID toolwndCommandID = new CommandID(GuidList.guidTwainsoft_SolutionRenamer_VSPackageCmdSet, (int)PkgCmdIDList.cmdidMyTool);
-                MenuCommand menuToolWin = new MenuCommand(OnShowToolWindow, toolwndCommandID);
+                var toolwndCommandID = new CommandID(GuidList.guidTwainsoft_SolutionRenamer_VSPackageCmdSet, (int)PkgCmdIDList.cmdidMyTool);
+                var menuToolWin = new MenuCommand(OnShowToolWindow, toolwndCommandID);
                 mcs.AddCommand( menuToolWin );
             }
 
-            //var service = GetService(typeof (SVsSolution)) as IVsSolution;
+            var service = GetService(typeof(SVsSolution)) as IVsSolution;
 
-            //uint cookie = 1;
-            //service.AdviseSolutionEvents(this, out cookie);
-            DTE2 = Package.GetGlobalService(typeof(SDTE)) as EnvDTE.DTE;
-            DTE2.Events.SolutionEvents.ProjectRenamed += SolutionEventsOnProjectRenamed;
-            DTE2.Events.SolutionEvents.Renamed += SolutionEventsOnRenamed;
+            uint cookie = 1;
+            service.AdviseSolutionEvents(this, out cookie);
+            //DTE2 = Package.GetGlobalService(typeof(SDTE)) as EnvDTE.DTE;
+            //DTE2.Events.SolutionEvents.ProjectRenamed += SolutionEventsOnProjectRenamed;
+            //DTE2.Events.SolutionEvents.Renamed += SolutionEventsOnRenamed;
         }
 
-        private void SolutionEventsOnRenamed(string oldName)
+        //private void SolutionEventsOnRenamed(string oldName)
+        //{
+        //    Debug.WriteLine("SolutionEventsOnRenamed");
+        //}
+
+        //public DTE DTE2 { get; set; }
+
+        //private void SolutionEventsOnProjectRenamed(Project project, string oldName)
+        //{
+        //    Debug.WriteLine("SolutionEventsOnProjectRenamed");
+        //}
+        public int OnAfterRenameProject(IVsHierarchy hierarchy)
         {
-            Debug.WriteLine("SolutionEventsOnRenamed");
+            object project;
+
+            ErrorHandler.ThrowOnFailure
+                (hierarchy.GetProperty(
+                    VSConstants.VSITEMID_ROOT,
+                    (int)__VSHPROPID.VSHPROPID_ExtObject,
+                    out project));
+
+            var p = project as Project;
+
+
+            return VSConstants.S_OK;
+            //return VSConstants.E_NOTIMPL;
+            //throw new NotImplementedException();
         }
 
-        public DTE DTE2 { get; set; }
-
-        private void SolutionEventsOnProjectRenamed(Project project, string oldName)
+        public int OnQueryChangeProjectParent(IVsHierarchy pHierarchy, IVsHierarchy pNewParentHier, ref int pfCancel)
         {
-            Debug.WriteLine("SolutionEventsOnProjectRenamed");
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnAfterChangeProjectParent(IVsHierarchy pHierarchy)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnAfterAsynchOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnBeforeCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.E_NOTIMPL;
+        }
+
+        public int OnAfterCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.E_NOTIMPL;
         }
     }
 }
