578f94c613d46bd603e62a282a4e14721ee6ebed
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-11-01 14:15:32 +0100

    Renamed the solution and project from SolutionRenamer to SimpleRenamer.

--- src/VSX/Twainsoft.SimpleRenamer.VSPackage/Resources/Resources.Designer.cs
+++ src/VSX/Twainsoft.SimpleRenamer.VSPackage/Resources/Resources.Designer.cs
@@ -8,7 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Twainsoft.SolutionRenamer.VSPackage.Resources {
+namespace Twainsoft.SimpleRenamer.VSPackage.Resources {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -36,7 +39,7 @@ namespace Twainsoft.SolutionRenamer.VSPackage.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Twainsoft.SolutionRenamer.VSPackage.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Twainsoft.SimpleRenamer.VSPackage.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
