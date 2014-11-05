353a83eb397dd2e214253e7e1ddfde7783409934
Fabian Deitelhoff <fabian@fabiandeitelhoff.de>
2014-11-05 20:44:22 +0100

    Small changes within the NLog config. Added the first rudimentary options dialog & store.

---
+++ src/VSX/Twainsoft.SimpleRenamer.VSPackage/GUI/Options/OptionsStore.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace Twainsoft.SimpleRenamer.VSPackage.GUI.Options
+{
+    [ClassInterface(ClassInterfaceType.AutoDual)]
+    [Guid("12D3C984-9150-481F-A0B8-4A37151FBD05")]
+    public class OptionsStore : DialogPage
+    {
+        [Category("Twainsoft SimpleRenamer")]
+        [DisplayName("Should the complete solution be compiled after the renaming was successfully?")]
+        [Description("Should the complete solution be compiled after the renaming was successfully?")]
+        public bool CompileSolutionAfterRenaming { get; set; }
+    }
+}
---
+++ src/VSX/Twainsoft.SimpleRenamer.VSPackage/GUI/Options/OptionsView.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace Twainsoft.SimpleRenamer.VSPackage.GUI.Options
+{
+    public partial class OptionsView : UserControl
+    {
+        private OptionsStore OptionsStore { get; set; }
+
+        private OptionsView()
+        {
+            InitializeComponent();
+        }
+
+        public OptionsView(OptionsStore optionsStore)
+            : this()
+        {
+            OptionsStore = optionsStore;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            
+        }
+    }
+}
